5155906928446132c75cf764c13fb78fc69688f0
rritik772 <54989885+rritik772@users.noreply.github.com>
2019-10-13 00:47:06 +0530

    Create spear of destiny.py

---
+++ spear of destiny.py
@@ -0,0 +1,14 @@
+print("""Jindagi sawar dun
+ik nai bahaar dun
+duniya hi badal dun mai to
+pyara sa chamatkar hun
+mai kisi ka sapna hun
+jo aaj ban gaya hai sach
+ab ye mera sapna hai ki
+sab ke sapne sach mai karoon
+aasma ko chunlun
+titli ban udun
+Han helicopter
+la la la la
+mai hun ik udta robo doraemon
+mai hun ik udta robo doraemon""")
